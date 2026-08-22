ac8ca482812e5ac3b79bf55beccf3b1d7ca2e908
SahuSujeeth <sahusujeeth2004@gmail.com>
2026-08-22 15:42:24 +0000

    new assignment

--- AdvC/Assignments/isalnum.c
+++ AdvC/Assignments/isalnum.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <ctype.h>
-// int isalnum(char ch)
-// {
-//     return char;
-// }
-int main ()
+
+char my_isalnum(char ch)
+{
+    if(ch >= '0' && ch <= '9' || ch>='a' && ch <= 'z' || ch>='A' && ch<='Z')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main()
 {
     char ch;
-    scanf("%c",&ch);
-    int res = isalnum(ch);
-    printf("%d\n",res);
+    int ret;
+    
+    printf("Enter the character:\n");
+    scanf("%c", &ch);
+    
+    ret = my_isalnum(ch);
+    /*
+        Based on return value, print whether ch is alphanumeric or not
+        
+    */
+    if (ret == 1)
+    {
+        printf("Entered character is alphanumeric character ");
+    }
+    else
+    {
+        printf("Entered character is not alphanumeric character");
+        
+    }
     return 0;
 }
